8f9251815b33adde879f954767271947b0082c5b
jakkcer <jakkcer.fivework@gmail.com>
2021-08-11 16:20:10 +0900

    ktlint導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id ("com.thinkimi.gradle.MybatisGenerator") version "2.3"
-	kotlin("jvm") version "1.5.20"
-	kotlin("plugin.spring") version "1.5.20"
+    id("org.springframework.boot") version "2.5.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.thinkimi.gradle.MybatisGenerator") version "2.3"
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+    kotlin("jvm") version "1.5.20"
+    kotlin("plugin.spring") version "1.5.20"
 }
 
 group = "com.springpractice"
@@ -13,37 +14,44 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.mybatis:mybatis:3.5.6")
-	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.mybatis:mybatis:3.5.6")
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 mybatisGenerator {
-	verbose = true
-	configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+    verbose = true
+    configFile = "$projectDir/src/main/resources/generatorConfig.xml"
 
-	dependencies {
-		mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
-		mybatisGenerator("mysql:mysql-connector-java:8.0.23")
-	}
+    dependencies {
+        mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
+        mybatisGenerator("mysql:mysql-connector-java:8.0.23")
+    }
+}
+
+ktlint {
+    enableExperimentalRules.set(true)
+    filter {
+        exclude("**/database/**")
+    }
 }
--- src/main/kotlin/com/springpractice/demo/GreeterController.kt
+++ src/main/kotlin/com/springpractice/demo/GreeterController.kt
@@ -1,6 +1,12 @@
 package com.springpractice.demo
 
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("greeter")
@@ -9,7 +15,7 @@ class GreeterController(
 ) {
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
-        return HelloResponse("Hello ${name}")
+        return HelloResponse("Hello $name")
     }
 
     @GetMapping("/hello/{name}")
