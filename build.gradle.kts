65a28979c0b2bfa53a2dd2365ad52704be1e8eb2
Jumpei Nitta <jakkcer.fivework@gmail.com>
2021-08-16 22:11:40 +0900

    第3回_20210816 (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.mybatis:mybatis:3.5.6")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.4")
     implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+    implementation("mysql:mysql-connector-java:8.0.23")
+    mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/springpractice/demo/database/UserDynamicSqlSupport.kt
+++ src/main/kotlin/com/springpractice/demo/database/UserDynamicSqlSupport.kt
@@ -1,7 +1,7 @@
 /*
  * Auto-generated file. Created by MyBatis Generator
  */
-package database
+package com.springpractice.demo.database
 
 import java.sql.JDBCType
 import org.mybatis.dynamic.sql.SqlTable
