ce080c9d6d7f801985fa6ef3dc103ceb57a6177c
jakkcer <jakkcer.fivework@gmail.com>
2021-08-23 21:07:34 +0900

    dockerとmysql

---
+++ database/testdata/02.sql
@@ -0,0 +1,7 @@
+INSERT INTO book VALUES
+    (100, 'Kotlin入門', 'コトリン太郎', '1950-10-01'),
+    (200, 'Java入門', 'ジャヴァ太郎', '2005-08-29');
+
+INSERT INTO user VALUES
+    (1, 'admin@test.com', '$2a$10$.kLvZAQfzNvFFlXzaQmwdeUoq2ypwaN.A/zbIQojZOY.7l8GGNy32', '管理者', 'ADMIN'),
+    (2, 'user@test.com', '$2a$10$dtB.bySf.ZcbOPOp3Q7ZgedqofClN56rQ6JboxBuiW02twNMcAoZS', 'ユーザー', 'USER');
